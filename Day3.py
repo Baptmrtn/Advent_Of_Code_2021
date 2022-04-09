f7d0cfc79981c3b7432aed995217fad95807bed2
Baptmrtn <78baptistemartin@gmail.com>
2022-04-09 16:11:01 +0200

    Day3, start

---
+++ Day3.py
@@ -0,0 +1,20 @@
+import numpy as np 
+
+numbers = [x.strip() for x in open('test.txt', 'r').readlines()]
+
+
+print(numbers)
+
+#power consumption
+
+#gamma rate 
+#epsilon rate
+
+def GammaRate():
+	pass
+
+def EpsilonRate():
+	pass
+
+def ConvertDecimal():
+	pass
