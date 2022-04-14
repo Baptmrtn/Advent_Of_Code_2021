b757670f9d62cb5c9214e58237c0a46ae2957aae
Baptmrtn <78baptistemartin@gmail.com>
2022-04-14 18:59:08 +0200

    Day3 first *

--- Day3.py
+++ Day3.py
@@ -1,20 +1,19 @@
 import numpy as np 
 
-numbers = [x.strip() for x in open('test.txt', 'r').readlines()]
+numbers = [x.strip() for x in open('Day3.txt', 'r').readlines()]
 
+def BinaryConvert(bit,index,rangeX):
+	return bit*2**(rangeX -1 - index)
 
-print(numbers)
+GammaRate = 0
+Epsilonrate = 0
 
-#power consumption
+for index in range(len(numbers[0])):
+	if sum([1 for x in numbers if x[index] == "1"])/len(numbers) > 0.5:
+		GammaRate += BinaryConvert(1,index,len(numbers[0]))
+		Epsilonrate += BinaryConvert(0,index,len(numbers[0]))
+	else:
+		GammaRate += BinaryConvert(0,index,len(numbers[0]))
+		Epsilonrate += BinaryConvert(1,index,len(numbers[0]))
 
-#gamma rate 
-#epsilon rate
-
-def GammaRate():
-	pass
-
-def EpsilonRate():
-	pass
-
-def ConvertDecimal():
-	pass
+print(GammaRate*Epsilonrate)
