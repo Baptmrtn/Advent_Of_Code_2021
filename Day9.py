4a71802ed42071ad930973720e971b6aac1b1c78
Baptmrtn <78baptistemartin@gmail.com>
2022-05-18 07:55:01 +0200

    Day9 First *

--- Day9.py
+++ Day9.py
@@ -15,65 +15,22 @@ class HeatMap():
 				currentValue = self.map[indexRow][indexCol]
 				checkMin = False
 				for com in ((0,1),(0,-1),(1,0),(-1,0)):
-					try:
-						if currentValue < self.map[indexRow + com[0]][indexCol+ com[1]]:
-							checkMin = True
-						else: 
-							checkMin = False
-							break	
-					except:
+					if indexRow + com[0] < 0 or indexCol+ com[1] < 0 :
 						pass
+					else:
+						try:
+							if currentValue < self.map[indexRow + com[0]][indexCol+ com[1]]:
+								checkMin = True
+							else: 
+								checkMin = False
+								break	
+						except:
+							pass
 				if checkMin:
-					self.ScorePart1 += currentValue
-					self.ScorePart1 += 1
-					print(f"{currentValue} at the position {indexRow} | {indexCol}")
+					self.ScorePart1 += currentValue + 1
 
 PATH = "Day9.txt"
 
 htmap = HeatMap(PATH)
 htmap.FindMinimum()
 print(htmap.ScorePart1)
-"""
-import numpy as np
-
-list2d = []
-
-with open(PATH, 'r') as f:
-    for line in f.readlines():
-        list2d.append(list(line.rstrip('\n')))
-
-array2d = np.array(list2d).astype(np.int32)
-# arr_mask = np.full(np.shape(array2d), -1)
-max_y, max_x = np.shape(array2d)
-
-total = 0
-for y in range(max_y):
-    for x in range(max_x):
-        debug_value = array2d[y][x]
-        count = 0
-        if x != 0:
-            if array2d[y][x] < array2d[y][x-1]:
-                count += 1
-        else:
-            count += 1
-        if x != max_x-1:
-            if array2d[y][x] < array2d[y][x+1]:
-                count += 1
-        else:
-            count += 1
-        if y != 0:
-            if array2d[y][x] < array2d[y-1][x]:
-                count += 1
-        else:
-            count += 1
-        if y != max_y-1:
-            if array2d[y][x] < array2d[y+1][x]:
-                count += 1
-        else:
-            count += 1
-        if count == 4:
-            # arr_mask[y][x] = array2d[y][x]
-            total += array2d[y][x] + 1
-
-print(total)
-"""
